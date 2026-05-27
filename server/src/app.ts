fee811b8a38b99ce524cd728198d6a406beb805f
inaemin <inaemin@gmail.com>
2026-05-27 19:55:21 +0900

    feat: errorHandler 미들웨어 분리
    Co-authored-by: yundol777 <yundol777@users.noreply.github.com>

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,26 +1,14 @@
-import express, { ErrorRequestHandler, response } from "express";
-import { createProduct } from "./controllers/products.controller.js";
-import { ZodError } from "zod";
-import { ERROR_RESPONSE } from "./constants/error.js";
+import express from "express";
+import {
+  createProduct,
+  getProducts,
+} from "./controllers/products.controller.js";
+import errorHandler from "./middlewares/errorHandler.js";
 
 const app = express();
 app.use(express.json());
 
-const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
-  if (error instanceof ZodError) {
-    const errorCode = error.issues[0]?.message as keyof typeof ERROR_RESPONSE;
-
-    response.status(400).json(ERROR_RESPONSE[errorCode]);
-    return;
-  }
-
-  response.status(500).json({
-    code: "INTERNAL_SERVER_ERROR",
-    message: "서버 내부 오류가 발생했습니다.",
-  });
-};
-
-app.get("/products", () => {});
+app.get("/products", getProducts);
 app.post("/products", createProduct);
 app.delete("/products/:id", () => {});
 
